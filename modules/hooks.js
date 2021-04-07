e0ab076514c7d391e743e3e87e4ed7054a53a01e
Argonius-Angelus <dragonwingeddestroyer@live.co.uk>
2021-04-08 00:50:43 +0100

    Update hooks.js
    Added config options for a user-defined clock directory.

--- modules/hooks.js
+++ modules/hooks.js
@@ -5,6 +5,14 @@ import { log } from "./util.js";
 Hooks.once("init", () => {
   log(`Init ${game.data.system.id}`);
   ClockSheet.register();
+  game.settings.register("lancer-clocks","extraPaths",{
+		name: 'Extra Lancer Clocks Path',
+		hint: 'This is the directory within the data path for custom clocks. This does not get created automatically and should be a folder that currently exists in the system.',
+		scope: 'client',
+		config: true,
+		type: String,
+		default: 'lancer-clocks',
+	});
 });
 
 Hooks.on("getSceneControlButtons", (controls) => {
